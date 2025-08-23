94ba944d3ad3fd47c1add773685678322b0ead4b
rahul kumar <rahul24srivastava2001@gmail.com>
2025-08-23 23:51:31 +0530

    Increase request body size limit for JSON and URL-encoded data in server.js to 50mb

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(helmet());
 app.use(cors());
 app.use(morgan("combined"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // MongoDB Connection
 // mongodb://newApi:NewsApi@147.93.110.206:27017 //main api
